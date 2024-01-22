0972c027c747d595f4aa9a736018caa3fa7b52cd
user <user@Lenovo-PC>
2024-01-22 23:02:22 +0100

    2ª semana

--- Prueba_git/src/main/java/Modelo/Mimodelo.java
+++ Prueba_git/src/main/java/Modelo/Mimodelo.java
@@ -4,6 +4,12 @@ public class Mimodelo {
 	
 	public class Mimodelo implements Mii_nterfaz{
 		
+		int numero1 =1;
+		int numero2 =4;
+		int operacion = numero1 + numero2;
+		int resultado = operacion;
+
+		return resultado;
 		
 		
 		
